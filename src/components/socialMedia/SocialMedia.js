2b3b5f9dd3f8ece8f8253363e5c23f9238faa0db
m-muiz <mmuizsiddiqui.com>
2023-09-13 14:27:41 +0500

    first commit

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -8,28 +8,28 @@ export default function socialMedia() {
   }
   return (
     <div className="social-media-div">
-      {socialMediaLinks.github ? (
-        <a
-          href={socialMediaLinks.github}
-          className="icon-button github"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fab fa-github"></i>
-          <span></span>
-        </a>
+      {socialMediaLinks.linkedin ? (
+      <a
+       href={socialMediaLinks.linkedin}
+       className="icon-button linkedin"
+       target="_blank"
+       rel="noopener noreferrer"
+       >
+       <i className="fab fa-linkedin-in"></i>
+       <span></span>
+       </a> 
       ) : null}
 
-      {socialMediaLinks.linkedin ? (
-        <a
-          href={socialMediaLinks.linkedin}
-          className="icon-button linkedin"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fab fa-linkedin-in"></i>
-          <span></span>
-        </a>
+      {socialMediaLinks.github ? (
+       <a
+       href={socialMediaLinks.github}
+       className="icon-button github"
+       target="_blank"
+       rel="noopener noreferrer"
+     >
+       <i className="fab fa-github"></i>
+       <span></span>
+     </a>
       ) : null}
 
       {socialMediaLinks.gmail ? (
@@ -130,3 +130,5 @@ export default function socialMedia() {
     </div>
   );
 }
+
+
